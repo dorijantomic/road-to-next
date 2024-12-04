9086dfbe50358647c0f3b81960124c8e792e773e
dorijantomic <dorijan11@gmail.com>
2024-12-04 21:09:10 +0100

    finish fallback routes

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,11 +1,7 @@
-import Link from "next/link";
-import React from "react";
+import { notFound } from "next/navigation";
 
 import { TicketItem } from "@/components/features/ticket/components/ticket-item";
 import { getTicket } from "@/components/features/ticket/queries/get-ticket";
-import { Placeholder } from "@/components/placeholder";
-import { buttonVariants } from "@/components/ui/button";
-import { ticketsPath } from "@/paths";
 
 type TicketPageProps = {
   params: {
@@ -15,20 +11,10 @@ type TicketPageProps = {
 
 const TicketPage = async ({ params }: TicketPageProps) => {
   const t = await getTicket(params.ticketId);
-  if (!t)
-    return (
-      <Placeholder
-        label="Ticket not found"
-        button={
-          <Link
-            href={ticketsPath()}
-            className={buttonVariants({ variant: "outline" })}
-          >
-            <h1 className="ml-2 text-lg font-semibold">Go to Tickets</h1>
-          </Link>
-        }
-      />
-    );
+  if (!t) {
+    notFound();
+  }
+
   return (
     <div className="flex justify-center animated-fade-in-from-top">
       <TicketItem ticket={t} isDetail />
