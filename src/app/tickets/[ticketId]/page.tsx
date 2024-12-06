f2b3343391ca71190bc9eefb77722d1549773cbd
dorijantomic <dorijan11@gmail.com>
2024-12-06 21:46:41 +0100

    finish connect to database section
    finish ORM section
    finish server queries section
    
    NOTE: This bricks the vercel auto deployment for sure

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -4,20 +4,21 @@ import { TicketItem } from "@/components/features/ticket/components/ticket-item"
 import { getTicket } from "@/components/features/ticket/queries/get-ticket";
 
 type TicketPageProps = {
-  params: {
+  params: Promise<{
     ticketId: string;
-  };
+  }>;
 };
 
 const TicketPage = async ({ params }: TicketPageProps) => {
-  const t = await getTicket(params.ticketId);
-  if (!t) {
+  const { ticketId } = await params;
+  const ticket = await getTicket(ticketId);
+  if (!ticket) {
     notFound();
   }
 
   return (
     <div className="flex justify-center animated-fade-in-from-top">
-      <TicketItem ticket={t} isDetail />
+      <TicketItem ticket={ticket} isDetail />
     </div>
   );
 };
