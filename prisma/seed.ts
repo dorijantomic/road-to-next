accf69318b0c0fd683440d975751b63de4e64148
dorijantomic <dorijan11@gmail.com>
2025-01-07 20:13:35 +0100

    add one to many relation between user and tickets

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,17 @@
+import { hash } from "@node-rs/argon2";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+const users = [
+  {
+    username: "admin",
+    email: "admin@admin.com",
+  },
+  {
+    username: "user",
+    email: "dorijan11@gmail.com",
+  },
+];
 const tickets = [
   {
     title: "Ticket 1",
@@ -29,7 +40,19 @@ const seed = async () => {
   console.log("DB Seed: Started...");
   const t0 = performance.now();
   await prisma.ticket.deleteMany();
-  await prisma.ticket.createMany({ data: tickets });
+  await prisma.user.deleteMany();
+
+  const passwordHash = await hash("secret");
+
+  const seededUsers = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({ ...user, passwordHash })),
+  });
+  await prisma.ticket.createMany({
+    data: tickets.map((ticket) => ({
+      ...ticket,
+      userId: seededUsers[0].id,
+    })),
+  });
   const t1 = performance.now();
   console.log(`DB Seed: Finished (${t1 - t0}ms)`);
 };
